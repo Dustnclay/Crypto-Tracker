becb17b8d4ca4315a6d3a9f7585fe9defdb981d1
Dustin <DustinTiner@Emilys-Air.home>
2021-11-20 10:44:01 -0800

    added window.location.origin to path

--- components/CoinInfo.js
+++ components/CoinInfo.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 export default function CoinInfo (props) {
     var coin = props.coin
-    var path = `coins/${coin}`
+    var path = window.location.origin+`/coins/${coin}` ||`coins/${coin}`
     return(
         <div className={styles.coin}>
             <Link             
